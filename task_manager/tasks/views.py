07e558d45615751cf62a415b86987b0598fbfaa9
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2024-12-15 19:08:07 +0500

    finish 7 step

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,14 +1,22 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django_filters.views import FilterView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
+from .filters import TaskFilter
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, FilterView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
+    filterset_class = TaskFilter
+
+    def get_filterset_kwargs(self, filterset_class):
+        kwargs = super().get_filterset_kwargs(filterset_class)
+        kwargs['request'] = self.request
+        return kwargs
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
